6132c35cecc427d20e49eaba353deb74f6b70d4a
Dan Starner <me@danstarner.com>
2023-02-23 23:42:52 -0500

    rebuilt homepage

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,6 +34,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
           editUrl: `https://github.com/${ORG}/${REPO}/tree/main/`,
         },
         blog: false,
