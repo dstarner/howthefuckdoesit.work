23e80ccf7b08b53ed5c5baabbf4526cf004ae0cc
Dan Starner <me@danstarner.com>
2023-02-24 00:15:32 -0500

    fixed repo name to have the '.' again

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 const ORG = "dstarner";
-const REPO = "howthefuckdoesitwork";
+const REPO = "howthefuckdoesit.work";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
